a74830dc0c9efca66bc37686915ff5523685a4d6
Han_Lin <Han_Lin@sis2.saison.co.jp>
2018-05-09 12:21:20 +0900

    ロト6実施一覧の並び順を変更

--- src/main/java/jp/co/saison/tvc/anythingok/repository/LotoMasterRepository.java
+++ src/main/java/jp/co/saison/tvc/anythingok/repository/LotoMasterRepository.java
@@ -2,16 +2,18 @@ package jp.co.saison.tvc.anythingok.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import jp.co.saison.tvc.anythingok.domain.LotoMaster;
 
 public interface LotoMasterRepository extends JpaRepository<LotoMaster, String> {
-    @Query("SELECT x FROM LotoMaster x ORDER BY x.loto_date")
+    @Query("SELECT x FROM LotoMaster x ORDER BY x.loto_date DESC")
     List<LotoMaster> findAll();
 
-//    @Query("SELECT x FROM LotoMaster x ORDER BY x.loto_date")
-//    Page<LotoMaster> findAllOrderByDate(Pageable pageable);
+    @Query("SELECT x FROM LotoMaster x ORDER BY x.loto_date DESC")
+    Page<LotoMaster> findAll(Pageable pageable);
 }
 
